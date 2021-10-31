bdf871a3cd771fb9c6a55fb54484616a2e841573
licheng58 <5627246+li_cheng58@user.noreply.gitee.com>
2021-10-31 21:34:27 +0800

    '222'

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,95 @@
 import React, { Component } from 'react'
-import { Button } from 'antd'
+import { Form, Input, Button, Checkbox, Spin } from 'antd'
 import logoImg from 'asserts/images/logo.png'
+import styles from './index.module.scss'
 
 class Login extends Component {
+  state = {
+    loading: false
+  }
   render() {
+    const { loading } = this.state
+
     return (
-      <div className="login">
-        <img src={logoImg} alt="" />
-        <Button type="primary">Primary Button</Button>
+      <div className={styles.login}>
+        <div className="login-form">
+          <Spin tip="登录中..." spinning={!!loading} className="login-spin">
+            <img src={logoImg} alt="" />
+            <Form
+              name="basic"
+              labelCol={{
+                span: 8
+              }}
+              wrapperCol={{
+                span: 16
+              }}
+              initialValues={{
+                remember: true
+              }}
+              onFinish={this.onFinish}
+              onFinishFailed={this.onFinishFailed}
+              autoComplete="off"
+            >
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your username!'
+                  }
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your password!'
+                  }
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item
+                name="remember"
+                valuePropName="checked"
+                wrapperCol={{
+                  offset: 8,
+                  span: 16
+                }}
+              >
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <Form.Item
+                wrapperCol={{
+                  offset: 8,
+                  span: 16
+                }}
+              >
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Spin>
+        </div>
       </div>
     )
+  }
+
+  onFinish = (values) => {
+    console.log('Success:', values)
+  }
+
+  onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo)
   }
 }
 
